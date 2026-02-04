12134f08ba919ab87065f001afefe71eaa58a36f
CleitonJnet <cleiton.jnet@gmail.com>
2026-02-04 11:20:40 -0300

    Ajuste do component deploy

--- resources/views/components/layouts/head/web/links.blade.php
+++ resources/views/components/layouts/head/web/links.blade.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
-@vite(['resources/css/tailwind.css', 'resources/css/styles.css', 'resources/js/javascript.js'])
-{{-- <link rel="stylesheet" href="{{ asset('build/assets/tailwind-BJ0PjdAM.css') }}">
-<link rel="stylesheet" href="{{ asset('build/assets/styles-Bvhe80wL.css') }}"> --}}
+{{-- @vite(['resources/css/tailwind.css', 'resources/css/styles.css', 'resources/js/javascript.js']) --}}
+<link rel="stylesheet" href="{{ asset('build/assets/tailwind-C8_T8fbC.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/styles-BzA4pOfC.css') }}">
 
 @stack('css')
 @livewireStyles
