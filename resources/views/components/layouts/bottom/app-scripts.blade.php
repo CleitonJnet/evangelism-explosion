97526d5c2a31905f895054c7b40074a304c8286e
CleitonJnet <cleiton.jnet@gmail.com>
2026-02-15 01:58:57 -0300

    scripts de css e js atualizados para deploy

--- resources/views/components/layouts/bottom/app-scripts.blade.php
+++ resources/views/components/layouts/bottom/app-scripts.blade.php
@@ -1,4 +1,4 @@
-{{-- <script type="module" src="{{ asset('build/assets/app-v1MpxStR.js') }}"></script> --}}
+<script type="module" src="{{ asset('build/assets/app-v1MpxStR.js') }}"></script>
 
 <!-- Swiper JS -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
