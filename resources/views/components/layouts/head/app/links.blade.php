4236f83924e70a9bdf1017790fb76fd2fa3f17c3
CleitonJnet <cleiton.jnet@gmail.com>
2026-03-02 17:06:48 -0300

    Atualização do banco e dados

--- resources/views/components/layouts/head/app/links.blade.php
+++ resources/views/components/layouts/head/app/links.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 @vite(['resources/css/tailwind.css', 'resources/css/app.css', 'resources/js/app.js'])
-{{-- <link rel="stylesheet" href="{{ asset('build/assets/tailwind-DS9F-r70.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('build/assets/tailwind-DL18tUTY.css') }}">
 <link rel="stylesheet" href="{{ asset('build/assets/app-Q49GAPoV.css') }}"> --}}
 
 @livewireStyles
